551036fcb4b8ff75d22438fe03850df724a0fcb5
Lola Rigaut-Luczak <rllola80@gmail.com>
2016-07-25 17:29:18 +0200

    modify address value in constant file

--- app/util/constants.js
+++ app/util/constants.js
@@ -1,5 +1,9 @@
 var Constants = (function() {
-  this.APP_ID = "17UnPjar4rB3TsvMrxzC5AQk2XGwn8axKi";
+  /*
+    Would be great if we could automatically retrieve the APP_ID
+    in the content.json file.
+  */
+  this.APP_ID = "1JfWHNDQeR71Uf8EtyRqCNJ1Ked5t1pukk";
 });
 
 module.exports = new Constants;
